aee6df304dc3b3769b93abed20d08673f3b59d2c
amir <owlython@gmail.com>
2020-03-08 18:31:05 +0330

    add main layout,logo_label and other changes

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ import sys
 import random
 from PySide2 import QtCore, QtWidgets, QtGui
 
+
 class MyWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle('KATANA')
+        self.setMinimumSize(1280, 768)
+        self.setWindowTitle('Game Club Managment System')
+        
+        self.lbl_logo = QtWidgets.QLabel('K A T A N A')
+        self.lbl_logo.setAlignment(QtCore.Qt.AlignCenter)
+        #self.content_layout = QtWidgets.QHBoxLayout()
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.addWidget(self.lbl_logo)
+        #self.main_layout.addWidget(self.content_layout)
+        self.setLayout(self.main_layout)
+        
         
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
